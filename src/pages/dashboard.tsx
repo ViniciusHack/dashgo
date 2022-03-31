81ea5b18eb82f0f21d53a62bda6242c20c3020da
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-03-31 00:31:51 -0300

    feature: Sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,16 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Siderbar } from "../components/Siderbar";
 
 
 export default function Dashboard() {
     return (
+      <Flex direction="column" h="100vh">
         <Header />
+
+        <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+          <Siderbar />
+        </Flex>
+      </Flex>
     )
 }
